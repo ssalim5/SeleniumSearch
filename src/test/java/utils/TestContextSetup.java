4807fd4c1dc75afd66b23cc05fdd6392929b60d0
Sakib Salim <sakibsalim5@gmail.com>
2024-01-23 00:50:11 -0500

    Window switch utility

--- src/test/java/utils/TestContextSetup.java
+++ src/test/java/utils/TestContextSetup.java
@@ -10,10 +10,12 @@ public class TestContextSetup {
 	public WebDriver driver;
 	public PageObjectManager pageObjectManager;
 	public TestBase testBase;
+	public GenericUtils genericUtils;
 	
 	public TestContextSetup() throws IOException {
 		testBase = new TestBase();
 		pageObjectManager = new PageObjectManager(testBase.WebDriverManager());
+		genericUtils = new GenericUtils(testBase.WebDriverManager());
 	}
 	
 }
